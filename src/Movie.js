43d30dad0b19cebcee24d8562323dce325059858
adriano.grimolizzi@symphony.com <adriano.grimolizzi@symphony.com>
2022-10-12 15:14:22 +0200

    replace switch with polymorphism - move getCharge to Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,29 @@
+const PriceCode = require('./PriceCode')
 class Movie {
     constructor(title, priceCode) {
         this.title = title
         this.priceCode = priceCode
+    }
+    getCharge(daysRented) {
+        let result = 0
+        switch (this.priceCode) {
+            case PriceCode.REGULAR:
+                result += 2
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5
+                }
+                break
+            case PriceCode.NEW_RELEASE:
+                result += daysRented * 3
+                break
+            case PriceCode.CHILDREN:
+                result += 1.5
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5
+                }
+                break
+        }
+        return result
     }
 }
 
--- src/Rental.js
+++ src/Rental.js
@@ -5,27 +5,7 @@ class Rental {
         this.movie = movie
         this.daysRented = daysRented
     }
-    getCharge() {
-        let result = 0
-        switch (this.movie.priceCode) {
-            case PriceCode.REGULAR:
-                result += 2
-                if (this.daysRented > 2) {
-                    result += (this.daysRented - 2) * 1.5
-                }
-                break
-            case PriceCode.NEW_RELEASE:
-                result += this.daysRented * 3
-                break
-            case PriceCode.CHILDREN:
-                result += 1.5
-                if (this.daysRented > 3) {
-                    result += (this.daysRented - 3) * 1.5
-                }
-                break
-        }
-        return result
-    }
+    getCharge = () => this.movie.getCharge(this.daysRented)
     getFrequentRenterPoints = () => (this.movie.priceCode === PriceCode.NEW_RELEASE && this.daysRented > 1) ? 2 : 1
 }
 

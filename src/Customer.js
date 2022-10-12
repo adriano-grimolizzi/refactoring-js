40360804b99d3216442335d563ea5018b78183cc
adriano.grimolizzi@symphony.com <adriano.grimolizzi@symphony.com>
2022-10-12 13:57:47 +0200

    move generateStatement() to Customer class

--- src/Customer.js
+++ src/Customer.js
@@ -8,6 +8,22 @@ class Customer {
     addRental = (rental) => {
         this.rentals.push(rental)
     }
+
+    generateStatement() {
+        let totalAmount = 0
+        let frequentRenterPoints = 0
+    
+        let result = `Rental Record for ${this.name}:\n`
+    
+        this.rentals.forEach(rental => {
+            totalAmount += rental.getCharge()
+            frequentRenterPoints += rental.getFrequentRenterPoints()
+        })
+        // add footer lines
+        result += `Amount owed is ${totalAmount}.\nYou earned ${frequentRenterPoints} frequent renter points.`
+    
+        return result
+    }
 }
 
 module.exports = Customer
--- src/__tests__/Customer.spec.js
+++ src/__tests__/Customer.spec.js
@@ -1,5 +1,7 @@
 const Customer = require('../Customer')
+const Movie = require('../Movie')
 const Rental = require('../Rental')
+const PriceCode = require('../PriceCode')
 
 describe('Customer', () => {
     it('should add a rental', () => {
@@ -9,5 +11,24 @@ describe('Customer', () => {
         expect(customer1.rentals.length).toBe(0)
         customer1.addRental(rental1)
         expect(customer1.rentals.length).toBe(1)
+    })
+
+    it('should generate a statement for a customer', () => {
+        const movie1 = new Movie('Breaking Bad', PriceCode.REGULAR)
+        const movie2 = new Movie('Better Call Saul', PriceCode.NEW_RELEASE)
+        const movie3 = new Movie('El Camino', PriceCode.CHILDREN)
+
+        const rental1 = new Rental(movie1, 4)
+        const rental2 = new Rental(movie2, 2)
+        const rental3 = new Rental(movie3, 6)
+
+        const customer1 = new Customer('James McGill')
+
+        customer1.addRental(rental1)
+        customer1.addRental(rental2)
+        customer1.addRental(rental3)
+
+        expect(customer1.generateStatement()).toEqual(
+            `Rental Record for James McGill:\nAmount owed is 17.\nYou earned 4 frequent renter points.`)
     })
 })
